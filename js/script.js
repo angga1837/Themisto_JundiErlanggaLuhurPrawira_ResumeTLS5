0e3b8169f0ebd3f4be97dad9ecd06ca8b00731b7
angga1837 <67214645+angga1837@users.noreply.github.com>
2025-09-20 09:37:55 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -228,3 +228,18 @@ function divisi(n){
 function closepopup(){
     popup.classList.remove("popup-visible");
 }
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, { threshold: 0.2 });
+
+reveals.forEach(reveal => {
+  observer.observe(reveal);
+});
+
